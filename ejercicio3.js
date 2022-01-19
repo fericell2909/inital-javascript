61ee1acdc60f91cca7b08c1cfde34bbb269d2b98
Marco Estrada Lopez <fericell2909@gmail.com>
2022-01-19 18:27:17 -0500

    ejercicio 3

---
+++ ejercicio3.js
@@ -0,0 +1,14 @@
+/*
+    Ejercicio 3 – creando un objeto
+
+*/
+
+var user = {
+    nombre : "Marco",
+    apellidos: "Estrada Lopez",
+    edad : 34
+}
+
+var nombrecompleto = "";
+
+nombrecompleto = user.nombre + " " +user.apellidos;
