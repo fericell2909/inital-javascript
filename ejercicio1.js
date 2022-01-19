17eec7668f66ec02311826b819345d9e52b020cc
Marco Estrada Lopez <fericell2909@gmail.com>
2022-01-19 12:08:03 -0500

    ejercicio1 - declaracion de variables

---
+++ ejercicio1.js
@@ -0,0 +1,18 @@
+/*
+    Ejercicio 1 – creando variables
+
+*/
+
+// Creando las variables 
+
+let Nombre, Apellidos , Ciudad,Edad;
+
+// Asignando variables
+
+Nombre = "Marco";
+Apellidos = "Estrada Lopez";
+Ciudad = "Lima"
+Edad = 34;
+
+
+
